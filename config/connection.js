b63d1fb8f2898d301161abd70b42a05b8758c375
ezemog1996 <enrique_g_gomez@hotmail.com>
2021-01-11 00:44:51 -0500

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "!UCSD_fall@2020!",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "!UCSD_fall@2020!",
+        database: "burgers_db"
+    });
+}
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-});
+connection.connect()
 
 module.exports = connection;
 
